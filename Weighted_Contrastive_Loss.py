ccb00fa6ac03a0526613018e9f4ab9a93290387f
Priyank Pathak <priyankpathak50@gmail.com>
2019-10-21 16:25:03 -0400

    Update Weighted_Contrastive_Loss.py

--- Weighted_Contrastive_Loss.py
+++ Weighted_Contrastive_Loss.py
@@ -5,13 +5,13 @@ import torch.nn as nn
 # https://arxiv.org/pdf/1811.01459v2.pdf
 
 class OSM_CAA_Loss(nn.Module):
-    def __init__(self, alpha=1.2, l=0.5, use_gpu=True):
+    def __init__(self, alpha=1.2, l=0.5, osm_sigma=0.8,use_gpu=True):
         super(OSM_CAA_Loss, self).__init__()
         self.use_gpu = use_gpu
-        self.alpha = 1.2 # margin of weighted contrastive loss, as mentioned in the paper 
-        self.l = 0.5 #  hyperparameter controlling weights of positive set and the negative set  
+        self.alpha = alpha # margin of weighted contrastive loss, as mentioned in the paper 
+        self.l = l #  hyperparameter controlling weights of positive set and the negative set  
         # I haven't been able to figure out the use of \sigma CAA 0.18 
-        self.osm_sigma = 0.8  #\sigma OSM (0.8) as mentioned in paper
+        self.osm_sigma = osm_sigma  #\sigma OSM (0.8) as mentioned in paper
         
     def forward(self, x, labels , embd):
         '''
